8d7d7560662b08c7a090aa080a8808eff42b5a9d
SirDuuck Creates <sir.fuuck666@gmail.com>
2026-07-28 20:06:35 -0300

    Primeira versão do projeto

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const botaoPostagem = document.getElementById("botaoPostagem");
+
+botaoPostagem.addEventListener("click", function() {
+    alert("Quack Quack! A área de publicações ainda está em manutenção.");
+});
